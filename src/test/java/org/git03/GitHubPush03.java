6a332b1749e43b9be1e3133a76351ec5e81adf20
infotech87871 <infotech87871@gmail.com>
2023-09-07 16:41:35 +0530

    First time Push

---
+++ src/test/java/org/git03/GitHubPush03.java
@@ -0,0 +1,14 @@
+package org.git03;
+
+public class GitHubPush03 {
+
+	private void orange() {
+		System.out.println("orange");
+	}
+	private void apple() {
+		System.out.println("apple");
+	}
+	private void grape() {
+		System.out.println("grape");
+	}
+}
